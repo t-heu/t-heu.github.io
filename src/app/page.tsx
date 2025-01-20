5bdc75b66f07d50603a25b3c2a5c77916893080b
Matheus G <teuzin375@gmail.com>
2025-01-20 10:38:19 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -190,7 +190,7 @@ export default function Home() {
               </Link>
               <Link
                 className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 border-gray-200 bg-white px-4 text-sm font-medium shadow-sm gap-2 transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-                href="https://t-heu.github.io/"
+                href="https://github.com/t-heu"
               >
                 <GithubIcon className="w-4 h-4" />
                 GitHub
